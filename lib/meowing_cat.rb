e22e09116a05323eb5538a7ecc6082a1844eec8d
Angela Appiah <alegnalawn2@yahoo.com>
2020-04-22 02:34:39 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,11 @@
-## code your solution here. 
+## code your solution here.
 class Cat
   attr_accessor (name)
   def (name)
-    @name 
+    @name
   end
   def name
-    @name= name  
+    @name= name
   end
   cat.new = "meow"
 end
